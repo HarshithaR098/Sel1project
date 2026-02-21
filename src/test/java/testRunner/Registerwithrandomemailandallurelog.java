b8aa6610449fe4ac6cf591ad79d16671b33330a6
harsh <harsh@harshitha>
2026-02-21 13:10:33 +0530

    next1

--- src/test/java/testRunner/Registerwithrandomemailandallurelog.java
+++ src/test/java/testRunner/Registerwithrandomemailandallurelog.java
@@ -13,7 +13,7 @@ import demoutils.AllureScreenshotUtil;
 import org.apache.logging.log4j.Logger; 
 
 public class Registerwithrandomemailandallurelog extends BaseTest {
-	private static final Logger log =  LogManager.getLogger(Registerwithrandomemailandallurelog.class);
+//	private static final Logger log =  LogManager.getLogger(Registerwithrandomemailandallurelog.class);
 
 	@Test //(retryAnalyzer = testListeners.RetryAnalyzer.class)
 	public void registerTest() {
@@ -22,22 +22,18 @@ public class Registerwithrandomemailandallurelog extends BaseTest {
 		homePage.clickRegister();
 		System.out.println("Clicked on Register link");
 		RegisterPage registerPage = new RegisterPage();
-		log.info("Filling the Registration form with emial: " + email);
+//		log.info("Filling the Registration form with emial: " + email);
 		AllureScreenshotUtil.attachLog("Filling the Registration form with emial: " + email);
 //			registerPage.registerUser("M","John", "Doe", AutoGeneratingEmail.email(), "Admin@123");
 		registerPage.registerUser("M", "Admin", "Dore", email, "admin@123");
 		String ER = "Your registration completed";
 		String AR = registerPage.getActualResult();
 		Assert.assertEquals(AR, ER);
-		log.info("Registration completed successfully with email: " + email);
+//		log.info("Registration completed successfully with email: " + email);
 		AllureScreenshotUtil.attachLog("Registration completed successfully with email: " + email);
 
 	}
 	
-	@Test
-	public void sampletest() {
-		log.info("This is a sample test to demonstrate retry analyzer");
-		Assert.assertEquals(true, false);
-	}
+	
 
 }
